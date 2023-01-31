7260962c2377cf3425e7019295ba2d9799310791
AkunneChidi <gc.akunne@gmail.com>
2023-01-31 19:27:37 +0100

    updated

--- .circleci/files/create.sh
+++ .circleci/files/create.sh
@@ -2,5 +2,5 @@
 aws cloudformation deploy \
     --template-file cloudfront.yml \
     --stack-name udapeople-cloudfront \
-    --parameter-overrides WorkflowID="7416-0977-8564" \
+    --parameter-overrides WorkflowID="08168262740" \
     --region=us-east-1
